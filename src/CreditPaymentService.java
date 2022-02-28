1416c6581abe18c647475ff6c556fd53a7ebc721
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-02-28 21:33:16 +0300

    changes

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,24 +1,10 @@
 public class CreditPaymentService {
-    public static final double loanRate = 1 + 9.99/100;
+    public static final double loanRate = 9.99/12/100;
+    public static int monthlyPayment;
 
-    public static int calculate (int creditSum, int time) {
-        time *= 12;
-        double rate = countRate(time);
-        double credit = creditSum * rate;
-        int monthlyPayment = (int)credit/time;
-        return monthlyPayment;
-    }
-
-    private static double countRate(int time) {
-        time *= 12;
-        double rate = 1 + 16.1432/100;
-
-        if (time <= 12) {
-            rate = 1 + 5.4932/100;
-        } else if (time <=24) {
-            rate = 1 + 10.736/100;
-        }
-
-        return rate;
+    public static int calculate (int creditSum, int months) {
+        return monthlyPayment = (int)(creditSum * ((loanRate*Math.pow((1+loanRate), months))
+                / (Math.pow(1+loanRate,months)-1)));
     }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
         int creditSum = 1_000_000;
 
-        int monthlyPayment = service.calculate(creditSum, 1);
+        int monthlyPayment = service.calculate(creditSum, 12);
 
-        int monthlyPayment2 = service.calculate(creditSum, 2);
+        int monthlyPayment2 = service.calculate(creditSum, 24);
 
-        int monthlyPayment3 = service.calculate(creditSum, 3);
+        int monthlyPayment3 = service.calculate(creditSum, 36);
 
         System.out.println(monthlyPayment);
         System.out.println(monthlyPayment2);
